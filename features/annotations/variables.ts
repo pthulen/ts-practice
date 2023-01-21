53ea9bc0af9383a9aff36883bbe090280e3fbc6c
Patrick Thulen <patrickthulen@Patricks-MBP.askey.com>
2023-01-20 16:06:49 -0800

    variable inference

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -28,3 +28,33 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+//when to use annotations
+// 1) Function that returns 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+console.log(coordinates);
+
+// 2) when we decalure a variable on one line
+// and initalize it later
+
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannon be inferred correctly
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
