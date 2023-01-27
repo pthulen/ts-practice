db531689c9d6aadf01535b97a1d21b94f55c44f6
pthulen <pfthulen@gmail.com>
2023-01-26 16:34:32 -0800

    added map markers

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,34 @@
+//instructions to every other class
+// how it can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+}
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,11 +1,12 @@
 /// <reference types="@types/google.maps" />
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { User } from "./User";
+import { Company } from "./Company";
 
-new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
+
+customMap.addMarker(user);
+customMap.addMarker(company);
