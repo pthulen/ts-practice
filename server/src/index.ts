6d8ba9978cd5da5b1fc8d12db1ebd7a52b0871d0
pthulen <pfthulen@gmail.com>
2023-01-31 14:56:14 -0800

    added basic auth route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("HEY!");
+import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
+import cookieSession from "cookie-session";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieSession({ keys: ["asdjkeva"] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
