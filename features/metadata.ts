63a1e868e4fa28ae87784cddc4193f07a3bd005e
pthulen <pfthulen@gmail.com>
2023-02-03 22:33:26 -0800

    metadata and decorators

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,19 +1,43 @@
 import "reflect-metadata";
 
-const plane = {
-  color: "red",
-};
+// const plane = {
+//   color: "red",
+// };
 
-Reflect.defineMetadata("note", "hi there", plane);
-Reflect.defineMetadata("height", "30k", plane);
-Reflect.defineMetadata("note2", "hi there again", plane, "color");
+// Reflect.defineMetadata("note", "hi there", plane);
+// Reflect.defineMetadata("height", "30k", plane);
+// Reflect.defineMetadata("note2", "hi there again", plane, "color");
 
-console.log(plane);
+// console.log(plane);
 
-const note2 = Reflect.getMetadata("note2", plane, "color");
-const note = Reflect.getMetadata("note", plane);
-const height = Reflect.getMetadata("height", plane);
+// const note2 = Reflect.getMetadata("note2", plane, "color");
+// const note = Reflect.getMetadata("note", plane);
+// const height = Reflect.getMetadata("height", plane);
 
-console.log(note);
-console.log(height);
-console.log(note2);
+// console.log(note);
+// console.log(height);
+// console.log(note2);
+
+@controller
+class Plane {
+  color: string = "red";
+
+  @get("/login")
+  fly(): void {
+    console.log("Vrrrr");
+  }
+}
+
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata("path", path, target, key);
+  };
+}
+
+function controller(target: typeof Plane) {
+  for (const key in target.prototype) {
+    const path = Reflect.getMetadata("path", target.prototype, key);
+    const middleware = Reflect.getMetadata("middleware", target.prototype, key);
+    router.get(path, middleware, target.prototype[key]);
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,16 @@ import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
 import cookieSession from "cookie-session";
 
+import { AppRouter } from "./AppRouter";
+import "./controllers/LoginController";
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieSession({ keys: ["asdjkeva"] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
