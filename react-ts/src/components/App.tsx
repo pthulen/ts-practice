72ea05c0b6c17a8f0f7703d4373ff2499971cb21
pthulen <pfthulen@gmail.com>
2023-02-22 13:55:58 -0800

    react-redux ts

---
+++ react-ts/src/components/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
